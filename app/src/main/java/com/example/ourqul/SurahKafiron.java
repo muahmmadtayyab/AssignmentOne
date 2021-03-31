4fdf77208d67c9547646899370e7204fb7566965
muhammadtayyab <mcsf19a037@pucit.edu.pk>
2021-04-01 01:04:44 +0500

    AssignmentOne Layout Updated

---
+++ app/src/main/java/com/example/ourqul/SurahKafiron.java
@@ -0,0 +1,15 @@
+package com.example.ourqul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurahKafiron extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surah_kafiron);
+        getSupportActionBar().setTitle("Surah Al-Kafiron");
+    }
+}
